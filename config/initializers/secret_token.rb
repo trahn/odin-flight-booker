649fdcd613f0cfb2166c2a922d7791315f50ddd6
Till Rahn <till.rahn@gmail.com>
2014-10-29 14:16:27 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OdinFlightBooker::Application.config.secret_key_base = '201088deebe1ba10f4bab9a21707dcf7f930254ae8fd87abea7b1464fd607388549c63d91d428ae39ed0e1dfbc735259921301cfdc1130e96fc5a4369065c66f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OdinFlightBooker::Application.config.session_store :cookie_store, key: '_odin-flight-booker_session'
